49af9f6342596dca12e69141daa1cd54b7babd87
kochiyama <marcelokochiyama2210@gmail.com>
2021-02-25 22:34:32 -0400

    create:page: home with topbar and left side components

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Head from 'next/head';
+
+import { ExperienceBar } from '../components/ExperienceBar';
+import Profile from '../components/Profile';
+import CompletedCheallenges from '../components/CompletedChallenges';
+import Countdown from '../components/Countdown';
+
+import styles from '../styles/pages/home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.it</title>
+      </Head>
+
+      <ExperienceBar />
+
+      <section>
+        <div className={styles.profile}>
+          <Profile />
+          <CompletedCheallenges />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
