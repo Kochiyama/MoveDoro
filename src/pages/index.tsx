220a9543f0edcceeeb8fefc8cf9d5f3b10a676c6
kochiyama <marcelokochiyama2210@gmail.com>
2021-03-02 03:59:31 -0400

    put navbar on dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import { ChallengeBox } from '../components/ChallengeBox';
 import styles from '../styles/pages/dashboard.module.css';
 import axios from 'axios';
 import api from '../config/api';
+import { NavBar } from '../components/NavBar';
 
 interface Avatar {
 	url: string;
@@ -56,6 +57,8 @@ export default function Dashboard(props: DashboardProps) {
 					</section>
 				</CountdownProvider>
 			</div>
+
+			<NavBar />
 		</ChallengesProvider>
 	);
 }
