a64a4f85a9706bc257765e6cc1e95c80d3e0195b
kochiyama <marcelokochiyama2210@gmail.com>
2021-03-05 10:10:27 -0400

    add Session Context to all the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { SessionProvider } from '../contexts/SessionsContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<SessionProvider>
+			<Component {...pageProps} />;
+		</SessionProvider>
+	);
 }
 
 export default MyApp;
