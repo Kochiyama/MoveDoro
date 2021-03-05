1fec09777c5ee193ba044d1ad8494705b1aa8fc8
kochiyama <marcelokochiyama2210@gmail.com>
2021-03-05 10:11:35 -0400

    since Session Context is valid on all app, use it locally is unnecessary

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -5,28 +5,25 @@ import { RegisterForm } from '../components/RegisterForm';
 import { SideBanner } from '../components/SideBanner';
 
 import styles from '../styles/pages/login&register.module.css';
-import { SessionContext, SessionProvider } from '../contexts/SessionsContext';
 
 export default function Home() {
 	return (
-		<SessionProvider>
-			<div className={styles.container}>
-				<SideBanner />
+		<div className={styles.container}>
+			<SideBanner />
 
-				<div className={styles.rightSideContainer}>
-					<h1>Bem-vindo</h1>
+			<div className={styles.rightSideContainer}>
+				<h1>Bem-vindo</h1>
 
-					<p>Crie sua conta gratuitamente.</p>
+				<p>Crie sua conta gratuitamente.</p>
 
-					<RegisterForm />
+				<RegisterForm />
 
-					<Link href='/login'>
-						<a className={styles.link}>
-							Já é cadastrado? Entre com suas cedenciais.
-						</a>
-					</Link>
-				</div>
+				<Link href='/login'>
+					<a className={styles.link}>
+						Já é cadastrado? Entre com suas cedenciais.
+					</a>
+				</Link>
 			</div>
-		</SessionProvider>
+		</div>
 	);
 }
