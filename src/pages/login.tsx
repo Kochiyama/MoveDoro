d182b8fcec9226f037a8cf13f921b007eac47e9f
kochiyama <marcelokochiyama2210@gmail.com>
2021-03-05 10:12:12 -0400

    extracted login handling to Sessions Context

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,48 +1,12 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import Cookie from 'js-cookie';
-import axios from 'axios';
 
 import { LoginForm } from '../components/LoginForm';
 import { SideBanner } from '../components/SideBanner';
 
 import styles from '../styles/pages/login&register.module.css';
-import { SessionModal } from '../components/SessionModal';
 
 export default function Login() {
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [isErrorModal, setIsErrorModal] = useState(false);
-	const [modalTitle, setModalTitle] = useState('');
-	const [modalMessage, setModalMessage] = useState('');
-
-	const route = useRouter();
-
-	function closeModal() {
-		setIsModalOpen(false);
-	}
-
-	async function login(values) {
-		try {
-			const response = await axios({
-				method: 'POST',
-				url: 'http://localhost:4000/sessions',
-				data: {
-					email: values.email,
-					password: values.password,
-				},
-			});
-
-			Cookie.set('movedoro_auth_token', String(response.data.token));
-			route.push('/');
-		} catch (error) {
-			setIsErrorModal(true);
-			setModalTitle(error.response.data.title);
-			setModalMessage(error.response.data.message);
-			setIsModalOpen(true);
-		}
-	}
-
 	return (
 		<div className={styles.container}>
 			<SideBanner />
@@ -52,7 +16,7 @@ export default function Login() {
 
 				<p>Faça login para começar</p>
 
-				<LoginForm handleSubmit={login} />
+				<LoginForm />
 
 				<Link href='/register'>
 					<a className={styles.link}>
@@ -60,15 +24,6 @@ export default function Login() {
 					</a>
 				</Link>
 			</div>
-
-			{isModalOpen && (
-				<SessionModal
-					closeModalFunction={closeModal}
-					isErrorModal={isErrorModal}
-					title={modalTitle}
-					message={modalMessage}
-				/>
-			)}
 		</div>
 	);
 }
